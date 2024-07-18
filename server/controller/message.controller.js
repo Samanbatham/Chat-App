09e1f65aa5d43c1fa046b49ed192a63fac5310b9
Samanbatham <batham15saman@gmail.com>
2024-07-18 14:52:52 +0530

    Server side first version

--- server/controller/message.controller.js
+++ server/controller/message.controller.js
@@ -27,9 +27,36 @@ try {
     if(newMessage){
         conversation.message.push(newMessage._id);
     }
+    // await conversation.save();
+    // await newMessage.save();
+
+    await Promise.all([conversation.save() , newMessage.save()]);
+
     res.status(201).json(newMessage)
 } catch (error) {
     console.log("Error in sendMessage controller", error.message)
         res.status(500).json({error:"Internal server error!!"})
 }
+}
+
+export const getMessage  = async(req,res)=>{
+  try {
+    
+    const {id:userToChatId} = req.params;
+    const senderId = req.user._id;
+    
+    const conversation = await Conversation.findOne({
+        participants: {$all: [senderId , userToChatId]}
+    }).populate("message")
+
+    const message = conversation.message;
+
+    console.log(message)
+
+    res.status(200).json(conversation.message)
+
+  } catch (error) {
+    console.log("Error in getMessage controller", error.message)
+        res.status(500).json({error:"Internal server error!!"})
+  }  
 }
