f0c2888a1ae8678a2c8489ca8665a7eebd1519d9
Samanbatham <batham15saman@gmail.com>
2024-07-19 18:09:28 +0530

    small changes in UI

--- public/src/components/message/MessageContainer.jsx
+++ public/src/components/message/MessageContainer.jsx
@@ -1,17 +1,41 @@
 import Messages from "./Messages.jsx";
 import MessageInput from "./MessageInput.jsx";
+import { HiOutlineChatAlt2 } from "react-icons/hi";
 
 const MessageContainer = () => {
+  const  noChatSelected = true;
   return (
     <div className="message-container">
+     {noChatSelected ?  <NoChatSelected /> :
+     
+      (
+        <>
       <div className="header">
         <span>To:</span> <span> Saman</span>
       </div>
 
       <Messages />
       <MessageInput />
+      </>
+    )}
+    
     </div>
   );
 };
 
 export default MessageContainer;
+
+
+const NoChatSelected = ()=>{
+  return(
+    <div className="nochat">
+      <div className="welcome">
+        <p>Welcome Saman</p>
+        <p>Select a chat to start messaging</p>
+        <p className="chat-icon">
+        <HiOutlineChatAlt2 />
+        </p>
+      </div>
+    </div>
+  )
+}
