b1c4a77e45acb54dd20e4daeca88a4649d4bb487
Samanbatham <batham15saman@gmail.com>
2024-07-21 17:10:53 +0530

    deployment v2

--- public/src/context/SocketContext.jsx
+++ public/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children})=>{
     
     useEffect(()=> {
         if(authUser){
-            const socket = io("https://chat-app-mum4.onrender.com" , {
+            const socket = io("https://chat-app-2f8g.onrender.com" , {
                 query:{
                     userId : authUser._id,
                 }
